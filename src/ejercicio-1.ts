133d61985696f40d705edfa1b58d6f1a5a9714a0
JonathanMartinezPerez <alu0101254098@ull.edu.es>
2025-03-09 21:52:36 +0000

    Ejercicio 1 mod

--- src/ejercicio-1.ts
+++ src/ejercicio-1.ts
@@ -1,24 +1,23 @@
 /**
  * @template T
- * @interface Streamable
- * @description Interfaz genérica que define las propiedades y métodos para una colección de emisiones.
+ * Interfaz genérica que define las propiedades y métodos para una colección de emisiones.
  */
 export interface Streamable<T> {
   /**
-   * @description Añade un nuevo elemento a la colección.
+   * Añade un nuevo elemento a la colección.
    * @param item El elemento a añadir.
    */
   add(item: T): void;
 
   /**
-   * @description Busca elementos en la colección por nombre.
+   * Busca elementos en la colección por nombre.
    * @param name El nombre a buscar.
    * @returns Una lista de elementos que coinciden con el nombre.
    */
   searchByName(name: string): T[];
 
   /**
-   * @description Busca elementos en la colección por año.
+   * Busca elementos en la colección por año.
    * @param year El año a buscar.
    * @returns Una lista de elementos que coinciden con el año.
    */
@@ -28,9 +27,7 @@ export interface Streamable<T> {
 /**
  * @template T
  * @abstract
- * @class BasicStreamableCollection
- * @implements {Streamable<T>}
- * @description Clase abstracta que implementa la interfaz Streamable.
+ * Clase abstracta que implementa la interfaz Streamable.
  */
 export abstract class BasicStreamableCollection<T> implements Streamable<T> {
   protected items: T[] = [];
@@ -54,17 +51,14 @@ export abstract class BasicStreamableCollection<T> implements Streamable<T> {
 }
 
 /**
- * @interface Series
- * @description Interfaz que define las propiedades de una serie.
+ * Clase que define las propiedades de una serie.
  */
 export class Series {
   constructor(public name: string, public year: number) {}
 }
 
 /**
- * @class SeriesCollection
- * @extends {BasicStreamableCollection<Series>}
- * @description Clase que modela una colección de series.
+ * Clase que modela una colección de series.
  */
 export class SeriesCollection extends BasicStreamableCollection<Series> {
   searchByName(name: string): Series[] {
@@ -77,17 +71,14 @@ export class SeriesCollection extends BasicStreamableCollection<Series> {
 }
 
 /**
- * @interface Movie
- * @description Interfaz que define las propiedades de una película.
+ * Clase que define las propiedades de una película.
  */
 export class Movie {
   constructor(public name: string, public year: number) {}
 }
 
 /**
- * @class MoviesCollection
- * @extends {BasicStreamableCollection<Movie>}
- * @description Clase que modela una colección de películas.
+ * Clase que modela una colección de películas.
  */
 export class MoviesCollection extends BasicStreamableCollection<Movie> {
   searchByName(name: string): Movie[] {
@@ -100,17 +91,14 @@ export class MoviesCollection extends BasicStreamableCollection<Movie> {
 }
 
 /**
- * @interface Documentary
- * @description Interfaz que define las propiedades de un documental.
+ * Clase que define las propiedades de un documental.
  */
 export class Documentary {
   constructor(public name: string, public year: number) {}
 }
 
 /**
- * @class DocumentariesCollection
- * @extends {BasicStreamableCollection<Documentary>}
- * @description Clase que modela una colección de documentales.
+ * Clase que modela una colección de documentales.
  */
 export class DocumentariesCollection extends BasicStreamableCollection<Documentary> {
   searchByName(name: string): Documentary[] {
